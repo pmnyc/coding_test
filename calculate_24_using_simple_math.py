9f87c3cb2a6ca9b54a4cdb1e80b45ccae6fec9f7
M <pmnyc@users.noreply.github.com>
2019-09-14 01:51:39 -0400

    calculate 24 using any numbers

---
+++ calculate_24_using_simple_math.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 14th, 2019
+
+This is the solution to calculating 24 based on sequence of numbers provided, and
+    it will only use +, -, *, / operations
+
+It needs Python 3
+
+@author: Man
+"""
+
+
+def solution(q, a, cache={}):
+    a = round(a, 8)
+    if len(q) <=1:
+        if q[0] == a:
+            cache[(q,a)] = str(a)
+        else:
+            cache[(q,a)] = None
+        return cache[(q, a)]
+    elif len(q)==2:
+        m1,m2 = q[0], q[1]
+        if m1+m2==a:
+            cache[(q,a)] = "("+str(m2)+ "+"+ str(m1)+")"
+        elif m2-m1==a:
+            cache[(q,a)] = "("+str(m2)+ "-"+ str(m1)+")"
+        elif m1-m2==a:
+            cache[(q,a)] = "("+str(m1)+ "-"+ str(m2)+")"
+        elif m2/m1==a:
+            cache[(q,a)] = "("+str(m2)+ "/"+ str(m1)+")"
+        elif m1/m2==a:
+            cache[(q,a)] = "("+str(m1)+ "/"+ str(m2)+")"
+        elif m2*m1==a:
+            cache[(q,a)] = "("+str(m2)+ "*"+ str(m1)+")"
+        else:
+            cache[(q,a)] = None
+        return cache[(q, a)]
+    else:
+        for i, v in enumerate(q):
+            q_ = tuple(list(q[:i]) + list(q[i+1:]))
+            if cache.get((q, a)) is None:
+                if (q_, a-v) not in cache:
+                    cache[(q_, a-v)] = solution(q_, a-v, cache=cache)
+                if cache.get((q_, a-v)) is not None:
+                    cache[(q, a)] = "(" + cache[(q_, a-v)] + "+" + str(v)+ ")"
+            if cache.get((q, a)) is None:
+                if (q_, v-a) not in cache:
+                    cache[(q_, v-a)] = solution(q_, v-a, cache=cache)
+                if cache.get((q_, v-a)) is not None:
+                    cache[(q, a)] = "(" + str(v) + "-" + cache[(q_, v-a)] + ")"
+            if cache.get((q, a)) is None:
+                if (q_, a*v) not in cache:
+                    cache[(q_, a*v)] = solution(q_, a*v, cache=cache)
+                if cache.get((q_, a*v)) is not None:
+                    cache[(q, a)] = "(" + cache[(q_, a*v)] + " / " + str(v) + ")"
+            if cache.get((q, a)) is None:
+                if (q_, a+v) not in cache:
+                    cache[(q_, a+v)] = solution(q_, a+v, cache=cache)
+                if cache.get((q_, a+v)) is not None:
+                    cache[(q, a)] = "(" + cache[(q_, a+v)] + " - " + str(v) + ")"
+            if cache.get((q, a)) is None:
+                if (q_, a/v) not in cache:
+                    cache[(q_, a/v)] = solution(q_, a/v, cache=cache)
+                if cache.get((q_, a/v)) is not None:
+                    cache[(q, a)] = "(" + cache[(q_, a/v)] + " * " + str(v) + ")"
+            if cache.get((q, a)) is None:
+                if (q_, v/a) not in cache:
+                    cache[(q_, v/a)] = solution(q_, v/a, cache=cache)
+                if cache.get((q_, v/a)) is not None:
+                    cache[(q, a)] = "(" + str(v) + "/" + cache[(q_, v/a)] + ")"
+            if cache.get((q, a)):
+                return cache[(q, a)]
+
+
+
+solution(q=(1,5,5,5), a=24)
