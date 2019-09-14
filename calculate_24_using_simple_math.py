71fcbe2b07acad7770ed9774259a35783a97dbbd
M <pmnyc@users.noreply.github.com>
2019-09-14 13:18:31 -0400

    update calculate 24 based on simple math operations

--- calculate_24_using_simple_math.py
+++ calculate_24_using_simple_math.py
@@ -1,25 +1,21 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Sat Sep 14th, 2019
-
 This is the solution to calculating 24 based on sequence of numbers provided, and
     it will only use +, -, *, / operations
 
 It needs Python 3
-
-@author: Man
+@author: M
 """
 
-
-def equivalent(a, b):
-    if abs(a-b) < 1e-8:
-        return True
-    else:
-        return False
-
-
-def solution(q, a, cache={}):
-    a = round(a, 10)
+def cal24_v1(q, a, cache={}):
+    def equivalent(a, b):
+        if abs(a-b) < 1e-15:
+            return True
+        else:
+            return False
+        a = round(a, 20)
     if len(q) <=1:
         if q[0] == a:
             cache[(q,a)] = str(a)
@@ -35,11 +31,11 @@ def solution(q, a, cache={}):
         elif equivalent(m1-m2, a):
             cache[(q,a)] = "("+str(m1)+ "-"+ str(m2)+")"
         elif equivalent(m2/m1, a):
-            cache[(q,a)] = "("+str(m2)+ "/"+ str(m1)+")"
+            cache[(q,a)] = str(m2)+ "/"+ str(m1)
         elif equivalent(m1/m2, a):
-            cache[(q,a)] = "("+str(m1)+ "/"+ str(m2)+")"
+            cache[(q,a)] = str(m1)+ "/"+ str(m2)
         elif equivalent(m2*m1, a):
-            cache[(q,a)] = "("+str(m2)+ "*"+ str(m1)+")"
+            cache[(q,a)] = str(m2)+ "*"+ str(m1)
         else:
             cache[(q,a)] = None
         return cache[(q, a)]
@@ -48,38 +44,47 @@ def solution(q, a, cache={}):
             q_ = tuple(list(q[:i]) + list(q[i+1:]))
             if cache.get((q, a)) is None:
                 if (q_, a-v) not in cache:
-                    cache[(q_, a-v)] = solution(q_, a-v, cache=cache)
+                    cache[(q_, a-v)] = cal24_v1(q_, a-v, cache=cache)
                 if cache.get((q_, a-v)) is not None:
                     cache[(q, a)] = "(" + cache[(q_, a-v)] + "+" + str(v)+ ")"
             if cache.get((q, a)) is None:
                 if (q_, v-a) not in cache:
-                    cache[(q_, v-a)] = solution(q_, v-a, cache=cache)
+                    cache[(q_, v-a)] = cal24_v1(q_, v-a, cache=cache)
                 if cache.get((q_, v-a)) is not None:
                     cache[(q, a)] = "(" + str(v) + "-" + cache[(q_, v-a)] + ")"
             if cache.get((q, a)) is None:
                 if (q_, a*v) not in cache:
-                    cache[(q_, a*v)] = solution(q_, a*v, cache=cache)
+                    cache[(q_, a*v)] = cal24_v1(q_, a*v, cache=cache)
                 if cache.get((q_, a*v)) is not None:
                     cache[(q, a)] = "(" + cache[(q_, a*v)] + " / " + str(v) + ")"
             if cache.get((q, a)) is None:
                 if (q_, a+v) not in cache:
-                    cache[(q_, a+v)] = solution(q_, a+v, cache=cache)
+                    cache[(q_, a+v)] = cal24_v1(q_, a+v, cache=cache)
                 if cache.get((q_, a+v)) is not None:
                     cache[(q, a)] = "(" + cache[(q_, a+v)] + " - " + str(v) + ")"
             if cache.get((q, a)) is None:
                 if (q_, a/v) not in cache:
-                    cache[(q_, a/v)] = solution(q_, a/v, cache=cache)
+                    cache[(q_, a/v)] = cal24_v1(q_, a/v, cache=cache)
                 if cache.get((q_, a/v)) is not None:
                     cache[(q, a)] = "(" + cache[(q_, a/v)] + " * " + str(v) + ")"
             if cache.get((q, a)) is None:
                 if (q_, v/a) not in cache:
-                    cache[(q_, v/a)] = solution(q_, v/a, cache=cache)
+                    cache[(q_, v/a)] = cal24_v1(q_, v/a, cache=cache)
                 if cache.get((q_, v/a)) is not None:
                     cache[(q, a)] = "(" + str(v) + "/" + cache[(q_, v/a)] + ")"
             if cache.get((q, a)):
                 return cache[(q, a)]
 
 
+def solution(q, a):
+    res = cal24_v1(q, a)
+    if res:
+        if res.startswith("("):
+            res = res[1:-1]
+    return res
 
+
+##################  Test Cases  ##################
 solution(q=(1,5,5,5), a=24)
+
 solution(q=(2,2,13,13), a=24)
